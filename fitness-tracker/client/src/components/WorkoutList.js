7cf3cce85ac0c412aa6882bc7fe490c5a3ba21d6
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-05-10 15:28:04 +1000

    init commit

--- fitness-tracker/client/src/components/WorkoutList.js
+++ fitness-tracker/client/src/components/WorkoutList.js
@@ -22,33 +22,3 @@ function WorkoutList({ workouts, fetchWorkouts, setEditingWorkout }) {
 }
 
 export default WorkoutList;
-
-
-
-
-
-
-/*import React from 'react';
-import axios from 'axios';
-
-function WorkoutList({ workouts, fetchWorkouts, setEditingWorkout }) {
-  const handleDelete = async (id) => {
-    await axios.delete(`http://localhost:5002/api/workouts/${id}`);
-    fetchWorkouts();
-  };
-
-  return (
-    <div>
-      <h3>Workout History</h3>
-      {workouts.map((w) => (
-        <div key={w.id}>
-          <strong>{w.date}</strong> - {w.type} - {w.duration} min - {w.calories} cal
-          <button onClick={() => setEditingWorkout(w)}>Edit</button>
-          <button onClick={() => handleDelete(w.id)}>Delete</button>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default WorkoutList;*/
--- fitness-tracker/server.js
+++ fitness-tracker/server.js
@@ -8,7 +8,6 @@ import { fileURLToPath } from "url";
 
 
 const app = express();
-//const PORT = process.env.PORT || 5002;
 const PORT = 5002;
 
 
@@ -19,7 +18,6 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
-//app.use('/', express.static('public'));
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/api/workouts", workoutRoutes);
@@ -27,5 +25,3 @@ app.use("/api/workouts", workoutRoutes);
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
-
-//"dev": "nodemon server.js"
